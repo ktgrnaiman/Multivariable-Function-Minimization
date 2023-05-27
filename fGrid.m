2bd25995401ed93817aa8dfab3c7fc181b5db551
Каримбаев Тагир Тимурович <kttnaiman0@gmail.com>
2023-05-27 17:39:22 +0300

    Fixing issue with switching directions in funciton values

--- fGrid.m
+++ fGrid.m
@@ -1,8 +1,8 @@
 function Z = fGrid(f, x, y)
-    Z = zeros(size(x,2), size(y,2));
-    for i=1:size(x,2)
-        for j=1:size(y,2)
-            Z(i, j) = f([x(i); y(j)]);
+    Z = zeros(length(x), length(y));
+    for i=1:length(y)
+        for j=1:length(x)
+            Z(i, j) = f([x(j); y(i)]);
         end
     end
     Z;

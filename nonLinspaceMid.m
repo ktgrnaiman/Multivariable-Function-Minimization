bdc7b2070190b29528d79db6688610c0fa2b8312
Каримбаев Тагир Тимурович <kttnaiman0@gmail.com>
2023-05-27 11:09:42 +0300

    Fixing problem with result vector size

--- nonLinspaceMid.m
+++ nonLinspaceMid.m
@@ -1,14 +1,11 @@
 function res = nonLinspaceMid(min, max, c, n)
     if mod(n,2) == 1
-        x = nonLinspace((max - min)/2, c, n/2);
+        x = nonLinspace((max - min)/2, c, (n+1)/2);
         xl = min + x;
         xr = max - wrev(x);
         res = [xl xr(2:end)];
     else
-        x = nonLinspace((max - min)/2, c, n/2 - 1);
-        xl = min + x;
-        xr = max - wrev(x);
-        res = [xl xr(2:end)];
+        error('Vector size must be odd number');
     end
 end
 
